18793beaf1ac77817dc795d3db27825065280e41
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-03 13:21:45 +0600

    setup complete

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 
 const MainLayout = () => {
   return (
     <div>
         <div className="grid-background"></div>
+        <main className=" min-h-screen container mx-auto">
+        <Navbar/>
         <Outlet/>
+        <Footer/>
+        </main>
     </div>
   )
 }
