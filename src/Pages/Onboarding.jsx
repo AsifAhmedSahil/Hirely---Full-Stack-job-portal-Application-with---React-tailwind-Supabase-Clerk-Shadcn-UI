4b693c68a4e80e34b370021b5fa8504a33c7cf6f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-06 15:15:41 +0600

    onboarding issue solve

--- src/Pages/Onboarding.jsx
+++ src/Pages/Onboarding.jsx
@@ -1,15 +1,41 @@
+import { Button } from "@/components/ui/button"
 import { useUser } from "@clerk/clerk-react"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import {BarLoader} from 'react-spinners'
 
 
 const Onboarding = () => {
   const {user,isLoaded} = useUser()
+  const navigate = useNavigate()
+
+  const handleRoleSelection = async(role) =>{
+    await user.update({
+      unsafeMetadata: {role}
+    })
+    .then(() =>{
+      navigate(role === "recruiter"? "/post-jobs":"/jobs")
+    })
+  }
+
+  useEffect(()=>{
+    if(user?.unsafeMetadata?.role){
+      navigate(user?.unsafeMetadata?.role === "recruiter"? "/post-jobs":"/jobs")
+    }
+  },[user])
 
   if(!isLoaded){
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7"/>
   }
   return (
-    <div>Onboarding</div>
+    <div className="flex flex-col justify-center items-center mt-40">
+      <h1 className="gradient-title text-7xl lg:text-8xl font-extrabold">I am a...</h1>
+      <div className="mt-16 w-full grid grid-cols-2 md:px-20 gap-4">
+        <Button variant="blue" className="h-36 text-2xl" onClick ={() =>handleRoleSelection("candidate")}>Candidate</Button>
+        <Button variant="destructive" className="h-36 text-2xl" onClick ={() =>handleRoleSelection("recruiter")}>Recruiter</Button>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,15 @@
 import { Link, useSearchParams } from "react-router-dom";
 // import { Button } from "./ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignIn,
-  
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignIn, UserButton, useUser } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  const [showSignIn,setShowSignIn] = useState(false)
+  const [showSignIn, setShowSignIn] = useState(false);
 
   const [search, setSearch] = useSearchParams();
-  // const { user } = useUser();
+  const { user } = useUser();
 
   useEffect(() => {
     if (search.get("sign-in")) {
@@ -23,59 +17,75 @@ const Navbar = () => {
     }
   }, [search]);
 
-  const handleOverlayClick = (e) =>{
-    if(e.target === e.currentTarget){
-      setShowSignIn(false)
-      setSearch({})
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
     }
-  }
+  };
 
   return (
     <div>
       <nav className="flex justify-between items-center p-4">
         <Link>
           {/* <img src="/logo.png" alt="logo" /> */}
-         <i> <h2 className="text-4xl font-bold text-blue-500">Hirely</h2></i>
+          <i>
+            {" "}
+            <h2 className="text-4xl font-bold text-blue-500">Hirely</h2>
+          </i>
         </Link>
 
         {/* <Button variant="outline">Login</Button> */}
-       <div className="flex gap-8">
-       <SignedOut>
-        <Button variant="outline" onClick={()=>setShowSignIn(true)}>Login</Button>
-        </SignedOut>
-        <SignedIn>
-        <Button variant="destructive" >
-          <PenBox size={20} className="mr-2"/>
-          Post a job</Button>
-          <UserButton 
-          appearance={{
-            elements:{
-              avatarBox: 'w-10 h-10'
-            }
-          }}
-          >
-            <UserButton.MenuItems>
-              <UserButton.Link label="My Jobs" labelIcon={<BriefcaseBusiness size={15}/>} href="/my-jobs"/>
-            </UserButton.MenuItems>
-            <UserButton.MenuItems>
-              <UserButton.Link label="Saved Jobs" labelIcon={<Heart size={15}/>} href="/saved-jobs"/>
-            </UserButton.MenuItems>
-
-          </UserButton>
-        </SignedIn>
-       </div>
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+           {user?.unsafeMetadata?.role === "recruiter" && <Link to={"/post-jobs"}>
+           <Button variant="destructive">
+              <PenBox size={20} className="mr-2" />
+              Post a job
+            </Button>
+           </Link>}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
       </nav>
 
-      {
-        showSignIn && (<div className="fixed  inset-0 flex items-center justify-center bg-black/50" onClick={handleOverlayClick}>
-            <SignIn 
+      {showSignIn && (
+        <div
+          className="fixed  inset-0 flex items-center justify-center bg-black/50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
             signUpForceRedirectUrl="/onboarding"
             fallbackRedirectUrl="/onboarding"
-            >
-
-            </SignIn>
-        </div>)
-      }
+          ></SignIn>
+        </div>
+      )}
     </div>
   );
 };
