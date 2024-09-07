c3b7ba34398346bec0beb1905e0dfa8581de440a
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-09-07 22:51:28 +0600

    saved job api

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,28 +1,57 @@
-import supabaseClient from "@/utils/supabase"
+import supabaseClient from "@/utils/supabase";
 
+export async function getJobs(token, { location, company_id, searchQuery }) {
+  const supabase = await supabaseClient(token);
 
-export  async function getJobs(token,{location,company_id,searchQuery}) {
-    const supabase = await supabaseClient(token)
+  let query = supabase
+    .from("jobs")
+    .select("* , company:companies(name,logo_url),saved:saved_jobs(id)");
 
-    let query =supabase.from("jobs").select("* , company:companies(name,logo_url),saved:saved_jobs(id)")
+  if (location) {
+    query = query.eq("location", location);
+  }
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
 
-    if(location){
-        query = query.eq("location",location)
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Error Fetching jobs", error);
+    return null;
+  }
+
+  return data;
+}
+export async function savedJobs(token, { alreadySaved }, saveData) {
+  const supabase = await supabaseClient(token);
+
+  if (alreadySaved) {
+    const { data, error: deleteError } = await supabase
+      .from("saved_jobs")
+      .delete()
+      .eq("job_id", saveData.job_id);
+
+    if (deleteError) {
+      console.error("Error deleting saved jobs", deleteError);
+      return null;
     }
-    if(company_id){
-        query = query.eq("company_id",company_id)
-    }
-    if(searchQuery){
-        query = query.ilike("title",`%${searchQuery}%`)
+
+    return data;
+  } else {
+    const { data, error: insertError } = await supabase
+      .from("saved_jobs")
+      .insert([saveData])
+      .select();
+
+    if (insertError) {
+      console.error("Error inserting saved jobs", insertError);
+      return null;
     }
 
-    const {data,error} = await query
-
-    if(error){
-        console.error("Error Fetching jobs",error)
-        return null
-    }
-
-    return data
-    
+    return data;
+  }
 }
